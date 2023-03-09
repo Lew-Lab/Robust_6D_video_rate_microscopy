a7aef1f9dc6ee9bfcf6bc1179dfb493e353fe4fd
Jasmine-Cheng <c.jasmine@wustl.edu>
2023-03-09 14:57:21 -0600

    update
    these updates may include some bugs.

--- main.py
+++ main.py
@@ -8,14 +8,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.io import loadmat, savemat
 import util.channel_2 as ch2
+import util.obj_func as obj
+import util.plot_func as plot
 
 ### CUDA setup ###
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('Using ' + device)
 
 ### document setup ###
-psf_file = 'ps2f_2c' # channel number x orientation x Z x X x Y
-object_file = 'test_circ' # Z x X x Y
+psf_file = 'dsf_raMVR' # channel number x orientation x Z x X x Y
+object_file = '' # orientation x Z x X x Y
 image_file = ''
 
 ### hyperparams setup ###
@@ -32,12 +34,26 @@ psf = loadmat(os.path.join('psf', psf_file+'.mat'))[psf_file]
 model = ch2.pixOL(psf, device)
 
 ### object domain ###
-obj = loadmat(os.path.join('object_plane', object_file+'.mat'))[object_file]
+object = obj.create_sphere(61,61,20,9,0.5)
+object = np.transpose(object, (0,3,1,2))
+# object = loadmat(os.path.join('object_plane', object_file+'.mat'))[object_file]
 
 ### image domain ###
 if image_file == '':
     img = model(object)
 
+plot.plot_img(img, 'image')
+
+initial = np.random.rand(*object.shape)
+img_est = model(initial)
+
 ### deconvolution ###
-obj_est, loss = ch2.estimate(model, obj, img, optim_param['learning_rate'], optim_param['max_iter'], optim_param['lambda_L1'], optim_param['lambda_TV'])
-print(loss)
+obj_est, loss = ch2.estimate(model, initial, img, optim_param['learning_rate'], optim_param['max_iter'], optim_param['lambda_L1'], optim_param['lambda_TV'])
+# print(loss)
+img_est = model(obj_est)
+
+obj_est = np.transpose(obj_est, (0,2,3,1))
+
+plot.plot_obj_voxel(obj_est,'z',0.5)
+
+plot.plot_img(img_est, 'Reconstructed image')
--- util/channel_2.py
+++ util/channel_2.py
@@ -7,23 +7,29 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
-class pixOL(nn.Module):
+class smolm(nn.Module):
     
     def __init__(self, psf, device):
-        super(pixOL, self).__init__()
+        super(smolm, self).__init__()
 
-        self.device = device
-        # setting up the PSF size
+        self.device = device # where the tensors saved
+        self.run_estimation = False # it's on when running estimation otherwise off
+
         self.channel = psf.shape[0]
-        # setting up the PSF size
+
+        # storing the size of the psf
+        self.psf_size = dict()
         self.psf_size['orientation'] = psf.shape[1]
         self.psf_size['depth'] = psf.shape[2]
         self.psf_size['width'] = psf.shape[3]
         self.psf_size['height'] = psf.shape[4]
+
+        # store the psf
         self.psf = torch.nn.Parameter(torch.tensor(psf, dtype=torch.float32, device=self.device), requires_grad=False)
 
     def forward(self, obj):
         
+        # storing the size of the object
         obj_size = dict()
         obj_size['orientation'] = obj.shape[0]
         obj_size['depth'] = obj.shape[1]
@@ -33,51 +39,91 @@ class pixOL(nn.Module):
         # we can change this later
         if obj_size != self.psf_size:
             raise Exception('Dimension of PSF and object is different.')
+
+            # max_depth = np.max([obj_size['depth'], self.psf_size['depth']])
+            # max_width = np.max([obj_size['width'], self.psf_size['width']])
+            # max_height = np.max([obj_size['height'], self.psf_size['height']])
+
+            # # match the dimension of the psf and obj by padding 0
+            # obj_padded = nn.functional.pad(obj, (0,max_depth-obj_size['depth'],max_width-obj_size['width'],max_height-obj_size['height']), 'const', 0)
+            # psf_padded = nn.functional.pad(self.psf, (0,max_depth-self.psf_size['depth'],max_width-self.psf_size['width'],max_height-self.psf_size['height']), 'const', 0)
         
-        obj = torch.tensor(obj, dtype=torch.float32, device=self.device)
+        if not torch.is_tensor(obj): 
+            obj = torch.tensor(obj, dtype=torch.float32, device=self.device, requires_grad=True)
         
-        # FFT: dimension need to be checked 
-        psf_fft = fft.fft2(fft.ifftshift(self.psf, (3,4)))
-        obj_fft = fft.fft2(fft.ifftshift(obj, (3,4)))
+        # simulate the image
+        psf_fft = fft.rfft2(fft.ifftshift(self.psf, (3,4)))
+        obj_fft = fft.rfft2(fft.ifftshift(obj, (2,3)))
         img_fft = psf_fft * obj_fft
-        img = torch.real(fft.ifft2(img_fft))
-        img = torch.sum(img, 2, keepdim=False)
+        img = fft.ifftshift(fft.irfft2(img_fft), (3,4))
         img = torch.sum(img, 1, keepdim=False)
+        img = torch.sum(img, 1, keepdim=False)
+
+        # not converting img to numpy array if we are running estimation
+        if not self.run_estimation:
+            img = img.detach().cpu().numpy()
 
         return img
     
 
-def estimate(model, obj, img_ture, lr, max_iter, lambda_L1, lambda_TV):
-    optimizer = torch.optim.Adam(obj, lr)
-    
-    # initialization
-    obj = torch.tensor(obj, dtype=torch.float32, device=model.device)
+def estimate(model, obj, img_true, lr, max_iter, lambda_L1, lambda_TV):
 
-    loss_lsq = []
-    loss_L1 = []
-    loss_TV = []
-    loss_total = []
+    model.run_estimation = True
+
+    # initialization
+    obj = torch.tensor(obj, dtype=torch.float32, device=model.device, requires_grad=True)
+    img_true = torch.tensor(img_true, dtype=torch.float32, device=model.device, requires_grad=False)
+    arr_loss_lsq = torch.zeros(max_iter)
+    arr_loss_L1 = torch.zeros(max_iter)
+    arr_loss_TV = torch.zeros(max_iter)
+    arr_loss_total = torch.zeros(max_iter)
+
+    # set up the optimizer
+    optimizer = torch.optim.Adam([obj], lr)
+
+    # least square loss function
+    least_sq = nn.MSELoss(reduction='mean').type(torch.float32)
 
     for i in range (max_iter): 
 
-        optimizer.zero_grad()
         img_est = model(obj)
 
         # compute the loss function
-        np.append(loss_lsq, nn.MSELoss(img_est, img_ture, reduction='sum'))
-        np.append(loss_L1, nn.L1Loss(obj))
-        np.append(loss_TV, nn.SmoothL1Loss(obj))
-        loss = loss_lsq[-1] + lambda_L1*loss_L1[-1] + lambda_TV*loss_TV[-1]
-        np.append(loss_total, loss)
+        loss_lsq = least_sq(img_est, img_true)
+        loss_L1 = loss_sparsity(obj)
+        loss_TV = loss_smoothness(obj)
+        loss = loss_lsq + lambda_L1*loss_L1 + lambda_TV*loss_TV
+
+        # store the loss
+        arr_loss_lsq[i] = loss_lsq
+        arr_loss_L1[i] = loss_L1
+        arr_loss_TV[i] = loss_TV
+        arr_loss_total[i] = loss
 
         # gradient descent
+        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
+    # end estimation
+    obj = obj.detach().cpu().numpy()
     loss_final = dict()
-    loss_final['total'] = loss_total[-1]
-    loss_final['lsq'] = loss_lsq[-1]
-    loss_final['L1'] = loss_L1[-1]
-    loss_final['TV'] = loss_TV[-1]
+    loss_final['total'] = arr_loss_total.detach().numpy()
+    loss_final['lsq'] = arr_loss_lsq.detach().numpy()
+    loss_final['L1'] = arr_loss_L1.detach().numpy()
+    loss_final['TV'] = arr_loss_TV.detach().numpy()
+
+    model.run_estimation = False
 
     return obj, loss_final
+
+def loss_sparsity(obj):
+    return torch.mean(torch.abs(obj))
+
+def loss_smoothness(obj):
+    dfdz = torch.abs(obj[:,1:,:,:]-obj[:,0:-1,:,:])
+    dfdx = torch.abs(obj[:,1:,:,:]-obj[:,0:-1,:,:])
+    dfdy = torch.abs(obj[:,1:,:,:]-obj[:,0:-1,:,:])
+    tv = (torch.mean(dfdz)+torch.mean(dfdx)+torch.mean(dfdy))/3
+    return tv
+    
