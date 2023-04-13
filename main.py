353b4132b1dec37beeb8a5b83b78aff8543ec24f
Jasmine-Cheng <c.jasmine@wustl.edu>
2023-04-13 14:12:41 -0500

    update

--- main.py
+++ main.py
@@ -17,23 +17,23 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('Using ' + device)
 
 ### document setup ###
-psf_file = 'planar_pixOL' # channel number x orientation x Z x X x Y
+psf_file = 'planar_MVR' # channel number x orientation x Z x X x Y
 object_file = 'circle' # orientation x Z x X x Y
 image_file = ''
 
 ### hyperparams setup ###
 optim_param = dict()
 optim_param['learning_rate'] = 0.01
-optim_param['max_iter'] = 300
-optim_param['lambda_L1'] = 0.035
-optim_param['lambda_TV'] = 0.015
+optim_param['max_iter'] = 500
+optim_param['lambda_L1'] = 0
+optim_param['lambda_TV'] = 0.0003
 
 ### PSF ###
 psf = loadmat(os.path.join('psf', psf_file+'.mat'))['dsf']
 
 ### object domain ###
-object = loadmat(os.path.join('object_plane', object_file+'.mat'))['object']
-plot.plot_obj_voxel(object,'z',0.5)
+object = 100*loadmat(os.path.join('object_plane', object_file+'.mat'))['object']
+plot.plot_obj_voxel(object,'z',0.5,'GT')
 
 ### model setup ###
 model_cpu = cpu.smolm(psf, object.shape)
@@ -53,7 +53,7 @@ plt.title('mxx+myy+mzz')
 plt.show()
 model_cpu_iso = cpu.smolm(psf_iso, object_iso.shape)
 img_iso = model_cpu_iso.forward(object_iso)
-plot.plot_img(img, '(Bxx+Byy+Bzz)/3 convolve with (mxx+myy+mzz)')
+plot.plot_img(img_iso, '(Bxx+Byy+Bzz)/3 convolve with (mxx+myy+mzz)')
 initial_iso = np.random.rand(*object_iso.shape)
 obj_est_iso, loss_iso = gpu.estimate(psf_iso, initial_iso, img_iso, 0.01, 300, 0, 0, device)
 img_est_iso = model_cpu_iso.forward(obj_est_iso)
@@ -72,5 +72,7 @@ obj_est, loss = gpu.estimate(psf, initial, img, optim_param['learning_rate'], op
 
 print(loss)
 img_est = model_cpu.forward(obj_est)
-plot.plot_obj_voxel(obj_est,'z',0.5,'Estimation of circle')
-plot.plot_img(img_est, 'Reconstructed image of circle')
+# plot.plot_obj_voxel(obj_est,'z',0.5,'Estimation of circle')
+# plot.plot_obj_voxel(obj_est,'z',0.3,'Estimation of circle')
+# plot.plot_obj_voxel(obj_est,'z',0.9,'Estimation of circle')
+# plot.plot_img(img_est, 'Reconstructed image of circle')
